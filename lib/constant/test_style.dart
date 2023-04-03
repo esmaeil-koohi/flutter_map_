d9fd1ed402d906843ca8a3950bd8d250650d8c12
esmaeil-koohi <esmaeil76koohi@gmail.com>
2023-04-03 20:31:21 +0430

    create proj

---
+++ lib/constant/test_style.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MyTextStyle{
+  MyTextStyle._();
+
+ static TextStyle button = GoogleFonts.vazirmatn(fontSize: 20);
+
+}
