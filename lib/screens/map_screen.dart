053aa6903d09728498d9b4c6b2b8562306f408bf
esmaeil-koohi <esmaeil76koohi@gmail.com>
2023-04-04 12:49:42 +0430

    update ui

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -3,6 +3,14 @@ import 'package:flutter_map_proj/constant/dimens.dart';
 import 'package:flutter_map_proj/constant/test_style.dart';
 import 'package:flutter_map_proj/widget/my_back_button.dart';
 
+class CurrentWidgetStates {
+  CurrentWidgetStates._();
+
+  static const stateSelectOrigin = 0;
+  static const stateSelectDestination = 1;
+  static const stateRequestDriver = 2;
+}
+
 class MapScreen extends StatefulWidget {
   const MapScreen({Key? key}) : super(key: key);
 
@@ -11,6 +19,8 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  List currentWidgetList = [CurrentWidgetStates.stateSelectOrigin];
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -20,23 +30,88 @@ class _MapScreenState extends State<MapScreen> {
             Container(
               color: Colors.blueGrey,
             ),
-             MyBackButton(
-               onPressed: () {
-               
-             },),
-            Positioned(
-                bottom: Dimens.large,
-                left: Dimens.large,
-                right: Dimens.large,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  child: Text('انتخاب مبدا', style: MyTextStyle.button,),
-                )),
+            MyBackButton(
+              onPressed: () {
+                if(currentWidgetList.length > 1){
+                  setState(() {
+                   currentWidgetList.removeLast();
+                  });
+                }
+              },
+            ),
+            currentWidget(),
           ],
         ),
       ),
     );
   }
+
+  Widget currentWidget(){
+    Widget widget = origin();
+
+    switch(currentWidgetList.last){
+      case CurrentWidgetStates.stateSelectOrigin:
+        widget = origin();
+        break;
+      case CurrentWidgetStates.stateSelectDestination:
+        widget = dest();
+        break;
+      case CurrentWidgetStates.stateRequestDriver:
+        widget = reqDriven();
+        break;
+    }
+    return widget;
+  }
+
+  Widget origin() {
+    return Positioned(
+        bottom: Dimens.large,
+        left: Dimens.large,
+        right: Dimens.large,
+        child: ElevatedButton(
+          onPressed: () {
+            setState(() {
+              currentWidgetList.add(CurrentWidgetStates.stateSelectDestination);
+            });
+          },
+          child: Text(
+            'انتخاب مبدا',
+            style: MyTextStyle.button,
+          ),
+        ));
+  }
+
+  Widget dest() {
+    return Positioned(
+        bottom: Dimens.large,
+        left: Dimens.large,
+        right: Dimens.large,
+        child: ElevatedButton(
+          onPressed: () {
+            setState(() {
+              currentWidgetList.add(CurrentWidgetStates.stateRequestDriver);
+            });
+          },
+          child: Text(
+            'انتخاب مقصد',
+            style: MyTextStyle.button,
+          ),
+        ));
+  }
+
+  Widget reqDriven() {
+    return Positioned(
+        bottom: Dimens.large,
+        left: Dimens.large,
+        right: Dimens.large,
+        child: ElevatedButton(
+          onPressed: () {
+          },
+          child: Text(
+            'درخواست راننده',
+            style: MyTextStyle.button,
+          ),
+        ));
+  }
+
 }
-
-
